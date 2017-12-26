55fd5b5b52f6e152c5a2a3b3f1d68edef1b27c2b
James Liang <james.liang@augmentum.com.cn>
2017-12-26 10:36:52 +0800

    optimized sort and refined coding style

--- algs/src/algs/sort/MergeSort.java
+++ algs/src/algs/sort/MergeSort.java
@@ -1,30 +1,33 @@
 package algs.sort;
 
+import java.util.Arrays;
+
 public class MergeSort {
+	private final static int CUTOFF = 8;
+
 	public static int[] sort(int[] array, int lo, int high) {
 		if (high <= lo)
 			return null;
+
+		if (high <= lo + CUTOFF) {
+			InsertionSort.sort(Arrays.copyOfRange(array, lo, high + 1));
+		}
 
 		int mid = lo + (high - lo) / 2;
 		int[] aux = new int[array.length];
 		sort(array, lo, mid);
 		sort(array, mid + 1, high);
-
-		if (array[mid + 1] >= array[mid]) {
-			return null;
-		}
-
 		return merge(array, aux, lo, mid, high);
-
 	}
 
 	public static int[] sortBU(int[] array) {
+
 		int length = array.length;
 		int[] aux = new int[length];
 
-		for (int i = 1; i < length; i = 2 * i) {
-			for (int lo = 0; lo < length - i; lo += 2 * i) {
-				merge(array, aux, lo, lo + i - 1, Math.min(lo + 2 * i - 1, length - 1));
+		for (int step = 1; step < length; step = 2 * step) {
+			for (int location = 0; location < (length - step); location += 2 * step) {
+				merge(array, aux, location, location + step - 1, Math.min(location + 2 * step - 1, length - 1));
 			}
 		}
 
@@ -32,6 +35,10 @@ public class MergeSort {
 	}
 
 	private static int[] merge(int[] array, int[] aux, int lo, int mid, int high) {
+		if (array[mid] <= array[mid + 1]) {
+			return array;
+		}
+
 		int length = array.length;
 
 		for (int i = 0; i < length; i++) {
@@ -41,9 +48,9 @@ public class MergeSort {
 		int leftPosition = lo;
 		int rightPosition = mid + 1;
 
-		for (int index = 0; index <= high; index++) {
+		for (int index = lo; index <= high; index++) {
 			if (leftPosition > mid) {
-				array[index] = aux[leftPosition++];
+				array[index] = aux[rightPosition++];
 			} else if (rightPosition > high) {
 				array[index] = aux[leftPosition++];
 			} else if (aux[rightPosition] < aux[leftPosition]) {
@@ -52,6 +59,23 @@ public class MergeSort {
 				array[index] = aux[leftPosition++];
 			}
 		}
+
+		// We can use the following way to save time of determine whether the left or
+		// right arrays are exhausted. But it will cause unstable results.
+		// for (int k = lo; k <= mid; k++) {
+		// aux[k] = a[k];
+		// }
+
+		// for (int k = mid + 1;k <= hi; k++) {
+		// aux[k] = a[hi - k + mid + 1];
+		// }
+
+		// int i = lo, j = hi;
+
+		// for (int k = lo; k <= hi; k++)
+		// if (aux[i] <= aux[j]) a[k] = aux[i++];
+		// else a[k] = aux[j--];
+		// }
 
 		assert (SortUtil.isSorted(array));
 		return array;
